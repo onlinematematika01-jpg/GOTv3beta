6c4a250f8f622b93a35870e9ae084260c7300a11
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-03-28 13:08:07 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,11 @@
+import asyncpg
+from config import DB_URL
 
+pool = None
+
+async def connect_db():
+    global pool
+    pool = await asyncpg.create_pool(DB_URL)
+
+async def get_pool():
+    return pool
