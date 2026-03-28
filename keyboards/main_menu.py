ae6e22709d85c55e8fc0ff32a9a8242fa7cabdbe
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-03-28 13:09:04 +0500

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,1 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def main_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📊 Profil")],
+        ],
+        resize_keyboard=True
+    )
